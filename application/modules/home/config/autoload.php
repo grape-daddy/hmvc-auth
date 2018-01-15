665d66c07563c89b77292016c95f9ccd7873e2db
Komsan Nurak <n.komsan@gmail.com>
2018-01-15 12:02:02 +0700

    First commit
    Codeigniter 3 & HMVC

---
+++ application/modules/home/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload = array(
+    'helper'    => array('url', 'form'),
+    'libraries' => array('email','form_validation'),
+);
